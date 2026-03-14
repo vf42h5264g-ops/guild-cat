fe529c3f2c9bf49fe04b73a44ed93aba505ae2eb
vf42h5264g-ops <vf42h5264g@privaterelay.appleid.com>
2026-03-14 18:04:11 +0900

    Update app.js

--- stable/app.js
+++ stable/app.js
@@ -136,7 +136,7 @@ const INVEST = {
     magic: 15,
   },
   shops: {
-    insure: { name: "保険組合", base: 0.025, var: 0.05, icon: "🛡" },
+    insure: { name: "さかな組合", base: 0.025, var: 0.05, icon: "🐟" },
     arms:   { name: "武具商会", base: 0.03,  var: 0.10, icon: "🗡" },
     trade:  { name: "交易商会", base: 0.03,  var: 0.20, icon: "🚢" },
     magic:  { name: "魔導研究所", base: 0.02, var: 0.50, icon: "🔮" },
