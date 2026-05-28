37dcc07447cf03cdb81658e70bea4a6ac268aa37
vf42h5264g-ops <vf42h5264g@privaterelay.appleid.com>
2026-05-28 09:21:48 +0900

    Update app.js

--- stable/app.js
+++ stable/app.js
@@ -4264,23 +4264,6 @@ function renderQuestTab() {
 
 </div>
 
-        <summary
-          style="
-            cursor:pointer;
-            font-weight:800;
-            padding:6px 0;
-          "
-        >
-          助っ人一覧
-        </summary>
-
-        <div style="margin-top:10px;">
-          ${helperRows || `<div class="dim">助っ人なし</div>`}
-        </div>
-
-      </details>
-    </div>
-
     ${
       alpacaOffer ? `
         <div class="panelCard">
